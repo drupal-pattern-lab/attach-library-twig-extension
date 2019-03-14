95654b1caef375c80023558c0523113486819983
Mike Herchel <mike@lullabot.com>
2019-03-14 11:35:42 -0400

    Process CSS files that exist within library.

--- pl_attach-library.function.php
+++ pl_attach-library.function.php
@@ -13,28 +13,46 @@ $function = new Twig_SimpleFunction('attach_library', function ($string) {
   // Find Library in libraries.yml file.
   $yamlFile = glob('*.libraries.yml');
   $yamlOutput = Yaml::parseFile($yamlFile[0]);
-  $scriptTags = [];
+  $libraryTags = [];
+
+  // Add appropriate HTML tag to $libraryTags array.
+  $add_library_tag = function ($library_tag) use (&$libraryTags) {
+    $stringLoader = \PatternLab\Template::getStringLoader();
+    $libraryTags[] = $stringLoader->render(["string" => $library_tag, "data" => []]);
+  };
+
+  // By default prefix paths with a /, but remove this for external JS
+  // as it would break URLs.
+  $build_path = function($options, $file) {
+    return (isset($options['type']) && $options['type'] === 'external') ? $file : "/$file";
+  };
 
   // For each item in .libraries.yml file.
-  foreach($yamlOutput as $key => $value) {
+  foreach($yamlOutput as $file => $value) {
 
     // If the library exists.
-    if ($key === $libraryName) {
-      $files = $yamlOutput[$key]['js'];
-      // For each file, create an async script to insert to the Twig component.
-      foreach($files as $key => $file) {
-        // By default prefix paths with a /, but remove this for external JS
-        // as it would break URLs.
-        $path_prefix = '/';
-        if (isset($file['type']) && $file['type'] === 'external') {
-          $path_prefix = '';
+    if ($file === $libraryName) {
+      $js_files = $yamlOutput[$file]['js'];
+
+      if (!empty($js_files)) {
+        // For each file, create an async script to insert to the Twig component.
+        foreach($js_files as $file => $options) {
+          $add_library_tag('<script data-name="reload" data-src="' . $build_path($options, $file) . '"></script>');
         }
-        $scriptString = '<script data-name="reload" data-src="' . $path_prefix . $key . '"></script>';
-        $stringLoader = \PatternLab\Template::getStringLoader();
-        $scriptTags[$key] = $stringLoader->render(array("string" => $scriptString, "data" => []));
-    	}
+      }
+
+      $css_groups = $yamlOutput[$file]['css'];
+
+      if (!empty($css_groups)) {
+        // For each file, create an async script to insert to the Twig component.
+        foreach($css_groups as $group => $files) {
+          foreach($files as $file => $options) {
+            $add_library_tag('<link rel="stylesheet" href="' . $build_path($options, $file) . '" media="all" />');
+          }
+        }
+      }
     }
   }
 
-  return implode($scriptTags);
+  return implode($libraryTags);
 }, array('is_safe' => array('html')));
